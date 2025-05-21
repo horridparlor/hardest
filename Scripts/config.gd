c7e6ef2b6dc8f53f5f18f4c11b6b616707f2a3dd
Eero Laine <eero.laine.posti@gmail.com>
2025-05-21 23:55:29 +0300

    Added song Shock the World

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -3,7 +3,7 @@ extends Node
 const VERSION : String = "0.3";
 
 const DEV_MODE : bool = true;
-const MAX_SONG_ID : int = 16;
+const MAX_SONG_ID : int = 17;
 const MAX_CARD_ID : int = 57;
 const WAIT_BEFORE_SONG_TO_REPEAT : int = min(MAX_SONG_ID / 2, 10);
 
@@ -20,7 +20,7 @@ const GUN_CHANCE : int = 1 if DEV_MODE else 1;
 const MIMIC_CHANCE : int = 1 if DEV_MODE else 1;
 const MUSIC_NIGHTCORE_PITCH : float = 1.2;
 
-const MUTE : bool = true if DEV_MODE else false;
+const MUTE : bool = false if DEV_MODE else false;
 const MUTE_MUSIC : bool = (false if DEV_MODE else false) or MUTE;
 const MUTE_SFX : bool = (false if DEV_MODE else false) or MUTE;
 const VOLUME : int = 0 if DEV_MODE else 0;

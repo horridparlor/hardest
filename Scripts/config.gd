cc72c80436540920c60365fbddc3cd1485612c0e
Eero Laine <eero.laine.posti@gmail.com>
2025-06-24 19:45:55 +0300

    Returned old soundtrack from yeterday

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -3,9 +3,9 @@ extends Node
 const VERSION : String = "0.6";
 
 const DEV_MODE : bool = false;
-const MAX_SONG_ID : int = 23;
+const MAX_SONG_ID : int = 21;
 const MAX_CARD_ID : int = 105; #120
-const WAIT_BEFORE_SONG_TO_REPEAT : int = min(MAX_SONG_ID - 2, 20);
+const WAIT_BEFORE_SONG_TO_REPEAT : int = min(MAX_SONG_ID - 3, 20);
 
 const AUTO_PLAY : bool = false if DEV_MODE else false;
 const AUTO_START : bool = false if DEV_MODE else false;

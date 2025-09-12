f2aa6d9164710cad947a95e52011943027233e77
Eero Laine <eero.laine.posti@gmail.com>
2025-09-13 01:06:22 +0300

    AI now recognized Gun TimeStopAdvantage

--- Scripts/System/enemy-ai.gd
+++ Scripts/System/enemy-ai.gd
@@ -206,6 +206,8 @@ static func get_card_truth(card : CardData, enemy : CardData, player : Player, g
 		player.trigger_chameleon(card);
 	if (card.has_ocean() or (enemy and enemy.has_ocean())) and !(gameplay.is_time_stopped or new_card.has_time_stop() or new_card.has_positive()):
 		System.AutoEffects.make_card_wet(card, gameplay, true, true, true);
+	if (card.has_time_stop() and card.is_gun()) and (is_already_on_field or !gameplay.is_time_stopped):
+		card.stopped_time_advantage = 2;
 	return card;
 
 static func get_first_face_up_card(source : Array) -> CardData:
